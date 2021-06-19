5d1494e15a9ad174a5bb3d9a8eacc4ca7d4f9456
some.one.who.codez <agmadaasa@gmail.com>
2021-06-19 16:56:29 +0100

    Updated for new package 📦

--- lib/main.dart
+++ lib/main.dart
@@ -39,41 +39,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   InterstitialAd myInterstitial;
-  bool hasFailed;
 
   @override
   void initState() {
     super.initState();
-    myInterstitial = InterstitialAd(
-      adUnitId: Platform.isAndroid
-          ? 'ca-app-pub-3940256099942544/1033173712'
-          : 'ca-app-pub-3940256099942544/4411468910', // test ad ids for differemt platform
-      request: AdRequest(),
-      listener: AdListener(
-        onAdLoaded: (ad) {
-          setState(() {
-            hasFailed = false;
-          });
-        },
-        onAdClosed: (ad) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => SecondPage(), // Navigate to second page
-            ),
-          );
-          ad.dispose(); // dispose of ad
-        },
-        onAdFailedToLoad: (ad, error) {
-          setState(() {
-            hasFailed = true;
-          });
-          ad.dispose(); // dispose of ad
-          print('Ad exited with error: $error');
-        },
-      ),
-    );
-    myInterstitial.load(); // loads ad before showing
+    _createInterstitialAd(); // create ad
   }
 
   @override
@@ -86,20 +56,65 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            hasFailed
-                ? Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          SecondPage(), // Navigate to second page
-                    ),
-                  )
-                : myInterstitial.show();
+            _showInterstitialAd(); // show ad
           },
           child: Text('Go To Second Page'),
         ),
       ),
     );
+  }
+
+  // create ad
+  _createInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: Platform.isAndroid
+          ? 'ca-app-pub-3940256099942544/1033173712'
+          : 'ca-app-pub-3940256099942544/4411468910', // test ad ids for different platforms
+      request: AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        // if ad fails to load
+        onAdFailedToLoad: (LoadAdError error) {
+          print('Ad exited with error: $error');
+        },
+
+        // else
+        onAdLoaded: (InterstitialAd ad) {
+          setState(() {
+            this.myInterstitial = ad; // set the ad equal to the current ad
+          });
+        },
+      ),
+    );
+  }
+
+  _showInterstitialAd() {
+    // create callbacks for ad
+    myInterstitial.fullScreenContentCallback = FullScreenContentCallback(
+      // when dismissed
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SecondPage(), // go to next page
+          ),
+        );
+        ad.dispose(); // dispose of ad
+      },
+
+      // if ad fails to show content
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SecondPage(), // go to next page
+          ),
+        );
+        print('$ad onAdFailedToShowFullScreenContent: $error'); // print error
+        ad.dispose(); // dispose ad
+      },
+    );
+
+    myInterstitial.show();
   }
 }
 
@@ -110,41 +125,11 @@ class SecondPage extends StatefulWidget {
 
 class _SecondPageState extends State<SecondPage> {
   InterstitialAd myInterstitial;
-  bool hasFailed;
 
   @override
   void initState() {
     super.initState();
-    myInterstitial = InterstitialAd(
-      adUnitId: Platform.isAndroid
-          ? 'ca-app-pub-3940256099942544/1033173712'
-          : 'ca-app-pub-3940256099942544/4411468910', // test ad ids for differemt platform
-      request: AdRequest(),
-      listener: AdListener(
-        onAdLoaded: (ad) {
-          setState(() {
-            hasFailed = false;
-          });
-        },
-        onAdClosed: (ad) {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => MyHomePage(), // Navigate to first page
-            ),
-          );
-          ad.dispose(); // dispose of ad
-        },
-        onAdFailedToLoad: (ad, error) {
-          setState(() {
-            hasFailed = true;
-          });
-          ad.dispose(); // dispose of ad
-          print('Ad exited with error: $error');
-        },
-      ),
-    );
-    myInterstitial.load(); // loads ad before showing
+    _createInterstitialAd(); // create ad
   }
 
   @override
@@ -184,23 +169,71 @@ class _SecondPageState extends State<SecondPage> {
         body: Center(
           child: ElevatedButton(
             onPressed: () {
-              if (hasFailed) {
-                Navigator.pop(context); // pops page
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        MyHomePage(), // replace popped page to call init again
-                  ),
-                );
-              } else {
-                myInterstitial.show();
-              }
+              _showInterstitialAd(); // show ad
             },
             child: Text('Go To First Page'),
           ),
         ),
       ),
     );
+  }
+
+  // create ad
+  _createInterstitialAd() {
+    InterstitialAd.load(
+      adUnitId: Platform.isAndroid
+          ? 'ca-app-pub-3940256099942544/1033173712'
+          : 'ca-app-pub-3940256099942544/4411468910', // test ad ids for different platforms
+
+      request: AdRequest(),
+
+      adLoadCallback: InterstitialAdLoadCallback(
+        // if ad fails to load
+        onAdFailedToLoad: (LoadAdError error) {
+          print('Ad exited with error: $error'); // print error
+        },
+
+        // else
+        onAdLoaded: (InterstitialAd ad) {
+          setState(() {
+            this.myInterstitial = ad; // set the ad equal to the current ad
+          });
+        },
+      ),
+    );
+  }
+
+  _showInterstitialAd() {
+    // create callbacks for ad
+    myInterstitial.fullScreenContentCallback = FullScreenContentCallback(
+      // when dismsissed
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+        Navigator.pop(context); // pop page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                MyHomePage(), // replace popped page to call init again
+          ),
+        );
+        ad.dispose(); // dispose ad
+      },
+
+      // if ad fails to show
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+        Navigator.pop(context); // pop page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                MyHomePage(), // replace popped page to call init again
+          ),
+        );
+        print('$ad onAdFailedToShowFullScreenContent: $error'); // print error
+        ad.dispose(); // dispose ad
+      },
+    );
+
+    myInterstitial.show(); // show ad
   }
 }
